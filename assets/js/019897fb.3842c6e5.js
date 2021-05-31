1895dbfc451957937b47f609048ca7022726f65b
Kalebu <isaackeinstein@gmail.com>
2021-05-31 17:15:07 +0300

    Deploy website - based on 0494decc425d33f73680120c1570df81e8baaa52

---
+++ assets/js/019897fb.3842c6e5.js
@@ -0,0 +1,1 @@
+(self.webpackChunkmtaa_doc=self.webpackChunkmtaa_doc||[]).push([[207],{1315:function(a){"use strict";a.exports=JSON.parse('{"allTagsPath":"/mtaa-docs/blog/tags","slug":"mtaa","name":"mtaa","count":1,"permalink":"/mtaa-docs/blog/tags/mtaa"}')}}]);
---
+++ assets/js/6dd0e2cc.de3832aa.js
@@ -0,0 +1,1 @@
+(self.webpackChunkmtaa_doc=self.webpackChunkmtaa_doc||[]).push([[997],{6353:function(t){"use strict";t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Integrating Mtaa with Flask","permalink":"/mtaa-docs/blog/mtaa with flask"}]}')}}]);
